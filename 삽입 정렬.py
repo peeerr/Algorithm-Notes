9971d11ebd16fed28243965f4c26bb79bc02e8db
JunHyeongChoi <ajfeh22@gmail.com>
2023-03-21 16:48:48 +0900

    Insertion Sort

---
+++ 삽입 정렬.py
@@ -0,0 +1,8 @@
+# 삽입정렬
+def insertion_sort(lst):
+    for i in range(1, len(lst)):
+        for j in range(i, 0, -1):  # i부터 1까지
+            if lst[j] < lst[j - 1]:
+                lst[j], lst[j - 1] = lst[j - 1], lst[j]
+            else:   # 더 작은 값 만나면
+                break
