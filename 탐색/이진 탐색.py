bb585bce4b18b78cce4023d4e59b137c711de5b8
JunHyeongChoi <ajfeh22@gmail.com>
2023-03-22 20:38:36 +0900

    Binary Search

--- 탐색/이진 탐색.py
+++ 탐색/이진 탐색.py
@@ -1,6 +1,6 @@
 # 이진 탐색
-def binary_search(lst, key, start, end):
-    if start > end:   # key가 없으면
+def binary_search(lst, key, start, end):  # start, end는 인덱스임에 주의
+    if start > end:  # key가 없으면
         return False
 
     mid = (start + end) // 2
@@ -8,6 +8,6 @@ def binary_search(lst, key, start, end):
     if lst[mid] == key:
         return mid
     elif lst[mid] > key:
-        return binary_search(lst, start, mid - 1, key)
+        return binary_search(lst, key, start, mid - 1)
     else:
-        return binary_search(lst, mid + 1, end, key)
+        return binary_search(lst, key, mid + 1, end)
